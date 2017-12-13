606220fe3dc9c09be9bc54fe12cc00de79a9d996
Josh Nussbaum <joshua.nussbaum@shopify.com>
2017-12-12 19:53:48 -0500

    Initial commit

---
+++ lib/xuber.ex
@@ -0,0 +1,18 @@
+defmodule XUber do
+  @moduledoc """
+  Documentation for XUber.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XUber.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xuber_test.exs
@@ -0,0 +1,8 @@
+defmodule XUberTest do
+  use ExUnit.Case
+  doctest XUber
+
+  test "greets the world" do
+    assert XUber.hello() == :world
+  end
+end
