0a673283141ae9e66fa9ce515ea0cd7ffc657080
Josh Nussbaum <joshnuss@gmail.com>
2017-12-31 02:53:06 -0500

    removed unused function

--- lib/xuber.ex
+++ lib/xuber.ex
@@ -1,18 +1,2 @@
 defmodule XUber do
-  @moduledoc """
-  Documentation for XUber.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> XUber.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
