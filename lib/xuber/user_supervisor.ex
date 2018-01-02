21d89fbde71616442d8d8a29282c5e2f74bd9cca
Josh Nussbaum <joshnuss@gmail.com>
2018-01-01 22:39:39 -0500

    Adds ability to start supervised actor for driver or passenger

--- lib/xuber/user_supervisor.ex
+++ lib/xuber/user_supervisor.ex
@@ -1,15 +1,20 @@
 defmodule XUber.UserSupervisor do
-  use Supervisor
+  use DynamicSupervisor
+
+  alias XUber.{User, Passenger, Driver}
 
   def start_link(_),
-    do: Supervisor.start_link(__MODULE__, :ok, name: XUber.UserSupervisor)
+    do: DynamicSupervisor.start_link(__MODULE__, :ok, name: XUber.UserSupervisor)
 
-  def init(:ok) do
-    children = [
-      XUber.Passenger,
-      XUber.Driver,
-    ]
+  def init(:ok),
+    do: DynamicSupervisor.init(strategy: :one_for_one)
 
-    Supervisor.init(children, strategy: :one_for_one)
-  end
+  def start_child(user=%User{type: :passenger}),
+    do: do_start_child({Passenger, user})
+
+  def start_child(user=%User{type: :driver}),
+    do: do_start_child({Driver, user})
+
+  defp do_start_child(child_spec),
+    do: DynamicSupervisor.start_child(__MODULE__, child_spec)
 end
