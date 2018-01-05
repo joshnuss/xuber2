fe970dbc045b7fb3bb3cf55464d029af83dafff3
Josh Nussbaum <joshua.nussbaum@shopify.com>
2018-01-04 21:18:32 -0500

    Adds logic to find pids within radius of search coordinates

--- lib/xuber/tile.ex
+++ lib/xuber/tile.ex
@@ -39,8 +39,15 @@ defmodule XUber.Tile do
     end
   end
 
-  def handle_call({:nearby, coordinates, radius, options}, _from, state) do
-    {:reply, Map.keys(state.pids), state}
+  def handle_call({:nearby, {lat1, lng1}, radius, options}, _from, state) do
+    results = state.pids
+              |> Enum.into([])
+              |> Enum.filter(fn {pid, %{position: {lat2, lng2}}} ->
+                :math.sqrt(:math.pow(lat2 - lat1, 2) + :math.pow(lng2 - lng1, 2)) < radius
+              end)
+              |> Enum.map(fn {pid, _position} -> pid end)
+
+    {:reply, {:ok, results}, state}
   end
 
   def handle_info({:DOWN, _ref, :process, pid, _reason}, state) do
