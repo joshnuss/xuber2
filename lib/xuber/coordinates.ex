62f865b0793ec7043f2bdb0804024fea7eb5c191
Josh Nussbaum <joshnuss@gmail.com>
2018-01-08 23:01:48 -0500

    Extract square function

--- lib/xuber/coordinates.ex
+++ lib/xuber/coordinates.ex
@@ -7,11 +7,14 @@ defmodule XUber.Coordinates do
   end
 
   def distance({x1, y1}, {x2, y2}) do
-    :math.sqrt(:math.pow(x2 - x1, 2) + :math.pow(y2 - y1, 2))
+    :math.sqrt(square(x2 - x1) + square(y2 - y1))
   end
 
   defp to_integer(n) when is_integer(n),
     do: n
   defp to_integer(float) when is_float(float),
     do: float |> Float.floor |> round
+
+  defp square(n),
+    do: :math.pow(n, 2)
 end
