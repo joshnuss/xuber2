51702c16c00934375c3900e1feb1cd5f5a409c3e
Josh Nussbaum <joshua.nussbaum@shopify.com>
2018-04-08 22:55:49 -0400

    Adds GenStateMachine

--- mix.exs
+++ mix.exs
@@ -22,6 +22,8 @@ defmodule XUber.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      #{:gen_state_machine, "~> 2.0"}
+      {:gen_state_machine, git: "https://github.com/joshnuss/gen_state_machine", branch: "add-child-spec"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
