7c55a05507889856f8256dbcae85762cdd101bc0
Josh Nussbaum <joshua.nussbaum@shopify.com>
2018-04-13 15:12:50 -0700

    Coverts Driver to use state machine

--- lib/xuber/driver.ex
+++ lib/xuber/driver.ex
@@ -1,5 +1,5 @@
 defmodule XUber.Driver do
-  use GenServer, restart: :transient
+  use GenStateMachine, restart: :transient
 
   alias XUber.{
     Grid,
@@ -10,77 +10,98 @@ defmodule XUber.Driver do
   }
 
   def start_link([user, coordinates]) do
-    state = %{
+    data = %{
       user: user,
       coordinates: coordinates,
       pickup: nil,
       passenger: nil,
       ride: nil,
-      status: :online,
     }
 
-    GenServer.start_link(__MODULE__, state, [])
+    GenStateMachine.start_link(__MODULE__, data, [])
   end
 
-  def init(state) do
-    Grid.join(self(), state.coordinates, [:driver])
+  def init(data) do
+    Grid.join(self(), data.coordinates, [:driver])
 
-    {:ok, state}
+    {:ok, :online, data}
   end
 
-  def handle_call(:available, _from, state=%{status: status}) when status != :driving and status != :dispatched,
-    do: {:reply, :ok, %{state | status: :available}}
+  def handle_event({:call, from}, :available, :online, data) do
+    reply = {:reply, from, :ok}
 
-  def handle_call(:unavailable, _from, state=%{status: :available}),
-    do: {:reply, :ok, %{state | status: :online}}
-
-  def handle_call(:offline, _from, state=%{status: status}) when status == :available or status == :online,
-    do: {:stop, :normal, :ok, state}
-
-  def handle_call({:dispatched, pickup, passenger}, _from, state) do
-    {:reply, :ok, %{state | pickup: pickup, passenger: passenger, status: :dispatched}}
+    {:next_state, :available, data, reply}
   end
 
-  def handle_call(:arrived, _from, state=%{pickup: pickup, passenger: passenger, status: :dispatched}) when not is_nil(pickup) do
-    Pickup.complete(pickup)
-    {:ok, ride} = RideSupervisor.start_child(passenger, self(), state.coordinates)
-    Passenger.depart(passenger, ride)
+  def handle_event({:call, from}, :unavailable, :available, data) do
+    reply = {:reply, from, :ok}
 
-    {:reply, :ok, %{state | ride: ride, pickup: nil, status: :riding}}
+    {:next_state, :online, data, reply}
   end
 
-  def handle_call(:dropoff, _from, state=%{status: :riding, ride: ride}) when not is_nil(ride) do
-    Ride.complete(ride)
-    {:reply, :ok, %{state | ride: nil, passenger: nil, status: :available}}
+  def handle_event({:call, from}, :offline, status, data) when status == :available or status == :online do
+    reply = {:reply, from, :ok}
+
+    {:stop, :normal, data, reply}
   end
 
-  def handle_call({:move, coordinates}, _from, state) do
-    Grid.update(self(), state.coordinates, coordinates)
+  def handle_event({:call, from}, {:dispatch, pickup, passenger}, :online, data) do
+    reply = {:reply, from, :ok}
+    new_data = %{data | pickup: pickup, passenger: passenger}
 
-    if state.pickup, do: Pickup.move(state.pickup, coordinates)
-    if state.ride, do: Ride.move(state.ride, coordinates)
+    {:next_state, :dispatched, new_data, reply}
+  end
 
-    {:reply, :ok, %{state | coordinates: coordinates}}
+  def handle_event({:call, from}, :arrived, :dispatched, data) do
+    :ok = Pickup.complete(data.pickup)
+    {:ok, ride} = RideSupervisor.start_child(data.passenger, self(), data.coordinates)
+    Passenger.depart(data.passenger, ride)
+
+    reply = {:reply, from, {:ok, ride}}
+    new_data = %{data | ride: ride, pickup: nil}
+
+    {:next_state, :riding, new_data, reply}
+  end
+
+  def handle_event({:call, from}, :dropoff, :riding, data=%{ride: ride}) when not is_nil(ride) do
+    :ok = Ride.complete(ride)
+
+    reply = {:reply, from, {:ok, ride}}
+    new_data = %{data | ride: nil, passenger: nil}
+
+    {:next_state, :available, new_data, reply}
+  end
+
+  def handle_event({:call, from}, {:move, coordinates}, _any, data) do
+    Grid.update(self(), data.coordinates, coordinates)
+
+    if data.pickup, do: Pickup.move(data.pickup, coordinates)
+    if data.ride, do: Ride.move(data.ride, coordinates)
+
+    reply = {:reply, from, :ok}
+    new_data = %{data | coordinates: coordinates}
+
+    {:keep_state, new_data, reply}
   end
 
   def offline(pid),
-    do: GenServer.call(pid, :offline)
+    do: GenStateMachine.call(pid, :offline)
 
   def available(pid),
-    do: GenServer.call(pid, :available)
+    do: GenStateMachine.call(pid, :available)
 
   def unavailable(pid),
-    do: GenServer.call(pid, :unavailable)
+    do: GenStateMachine.call(pid, :unavailable)
 
   def dispatch(pid, pickup, passenger),
-    do: GenServer.call(pid, {:dispatched, pickup, passenger})
+    do: GenStateMachine.call(pid, {:dispatch, pickup, passenger})
 
   def arrive(pid),
-    do: GenServer.call(pid, :arrived)
+    do: GenStateMachine.call(pid, :arrived)
 
   def dropoff(pid),
-    do: GenServer.call(pid, :dropoff)
+    do: GenStateMachine.call(pid, :dropoff)
 
   def move(pid, coordinates),
-    do: GenServer.call(pid, {:move, coordinates})
+    do: GenStateMachine.call(pid, {:move, coordinates})
 end
